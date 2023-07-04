d16cf0da3db1eb3767486c66eded65fb5a20035c
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-04 21:29:44 +0200

    Seeder za popunjavanje baze

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use \App\Models\User;
+use \App\Models\Manufacturer;
+use \App\Models\Smartphone;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,11 +15,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Manufacturer::truncate();
+        User::truncate();
+        Smartphone::truncate();
+
+        $user = User::factory()->create();
+
+        $man1 = Manufacturer::create([
+            'naziv'=>'Apple',
+            'zemlja_porekla'=>'USA'
+        ]);
+        $man2 = Manufacturer::create([
+            'naziv'=>'Samsung',
+            'zemlja_porekla'=>'Juzna Koreja'
+        ]);
+        $man3 = Manufacturer::create([
+            'naziv'=>'Huawei',
+            'zemlja_porekla'=>'Kina'
+        ]);
+
+
+        $ph1 = Smartphone::create([
+            'model'=>'Galaxy Z flip 4',
+            'serijski_broj'=>'133A3',
+            'memorija'=>'128 GB',
+            'cena'=>590,
+            'user_id'=>$user->id,
+            'manufacturer_id'=>$man2->id
+        ]);
+
+        $ph2 = Smartphone::create([
+            'model'=>'Iphone 14',
+            'serijski_broj'=>"a345d",
+            'memorija'=>'128 GB',
+            'cena'=>900,
+            'user_id'=>$user->id,
+            'manufacturer_id'=>$man1->id
+        ]);
+
+        $ph3 = Smartphone::create([
+            'model'=>'Huawei P50 pro',
+            'serijski_broj'=>"mb456",
+            'memorija'=>'256 GB',
+            'cena'=>800,
+            'user_id'=>$user->id,
+            'manufacturer_id'=>$man3->id
+        ]);
+
+
     }
 }
