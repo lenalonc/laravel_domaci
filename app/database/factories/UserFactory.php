c794c6c26d33d1647e8fa4c260238e6cf9bf075c
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-04 15:51:57 +0200

    Factories i seederi

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -18,10 +18,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'ime' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'adresa'=>fake()->address(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'lozinka' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
